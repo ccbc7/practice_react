2586ffa8463d033b4e08871b0a2d60286e109037
ccbc7 <flexibleelbow@gmail.com>
2024-09-15 13:13:53 +0900

    イベントやスタイルの使い方

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export const App = () => {
+  const onClickButton = () => {
+    alert();
+  };
+  const contentStyle = {
+    color: "blue",
+    fontSize: "18px", //font-sizeな度はキャメルケースに変換
+  };
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 style={{ color: "red" }}>こんにちは</h1>
+      <h1 style={contentStyle}>こんばんは</h1>
+      {console.log("App")}
+      <button onClick={onClickButton}>ボタン</button>
     </>
-  )
-}
-
-export default App
+  );
+};
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
+import { App } from "./App";
 
-createRoot(document.getElementById('root')).render(
+// index.htmlからid(root)を取得
+const rootElement = document.getElementById("root");
+
+// rootElementを使用してReactのルートを作成
+const root = createRoot(rootElement);
+
+// const App = () => <h1>こんにちは!</h1>;
+
+// ルートにReactコンポーネントをレンダリング
+root.render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
